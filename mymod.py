c34e9f94e86e1c2a33ec787b511846e9f3802db5
Richard Yim <turkeyslive@gmail.com>
2020-03-28 19:13:30 -0700

    adding new files

---
+++ mymod.py
@@ -0,0 +1,11 @@
+def billy_sequence(n):
+    
+    """ So it goes. """
+    x = []
+    for i in range(n):
+        x.append(0)
+     
+    return x
+
+if __name__== "__main__":
+    print("This is being run in an interpreter")
